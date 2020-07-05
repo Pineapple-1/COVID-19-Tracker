aab00620ed06badb5408fecb0801315cc8347a95
abdulrehman ajmal <abdulrehman.ajmal@outlook.com>
2020-07-05 07:17:29 +0500

    Updates

---
+++ precache-manifest.1f413c297e648ba5d2e89d49cdefe369.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0caed7baab149a1b18072c24633e0535",
+    "url": "/COVID-19-Tracker/index.html"
+  },
+  {
+    "revision": "10ccc56fddefe69bffa4",
+    "url": "/COVID-19-Tracker/static/css/main.82ec7c00.chunk.css"
+  },
+  {
+    "revision": "1b65c93fb5ad6f267c0a",
+    "url": "/COVID-19-Tracker/static/js/2.725f60c7.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/COVID-19-Tracker/static/js/2.725f60c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10ccc56fddefe69bffa4",
+    "url": "/COVID-19-Tracker/static/js/main.fe083a52.chunk.js"
+  },
+  {
+    "revision": "fd426e510d1d8479addb",
+    "url": "/COVID-19-Tracker/static/js/runtime-main.a251aa05.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/COVID-19-Tracker/static/media/image.d7265326.png"
+  }
+]);
